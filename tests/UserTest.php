2106745b771ad39b57ffd7d446fdef3b7d3767e7
Luis Montoya <luismec90@gmail.com>
2016-01-10 17:56:10 -0500

    Add User Test

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,13 +6,26 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseTransactions;
+
+    /** @test */
+    public function it_can_register()
     {
-        $this->assertTrue(true);
+        $this->visit('/register')
+            ->type('John Doe', 'name')
+            ->type('john@exmple.com', 'email')
+            ->type('123456', 'password')
+            ->type('123456', 'password_confirmation')
+            ->press('Register')
+            ->seePageIs('home');
+    }
+
+    public function it_can_login()
+    {
+        $this->visit('/login')
+            ->type('john@exmple.com', 'email')
+            ->type('123456', 'password')
+            ->press('Login')
+            ->seePageIs('home');
     }
 }
